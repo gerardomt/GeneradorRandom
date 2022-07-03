9fa18eaa609cb2d1577276b9dafc73d01eed3e10
Gerardomt <gerardomt@ciencias.unam.mx>
2022-07-03 11:39:55 -0500

    Primera versión de API generador_random

---
+++ src/generador_random.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+API para trabajar con la generación de números random
+"""
+
+
+import random
+
+def genera_entero_random(lim_inf, lim_sup):
+    """Devuelve un número al azar entre lim_inf y lim_sup incluyendo ambos
+    números
+
+    """
+    return random.randint(lim_inf, lim_sup)
