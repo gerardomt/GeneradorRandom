c1cf759a2caef053bd340b04ccd35dd9a378bd77
Gerardomt <gerardomt@ciencias.unam.mx>
2022-07-03 22:21:49 -0500

    Se completó archivo main que conecta el ui

---
+++ src/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QWindow
+import ui
+import api
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = ui.generador_random.Ui_NumeroRandom()
+        self.ui.setupUi(self)
+        self.ui.genera_button.clicked.connect(self.random_int)
+
+    def random_int(self):
+        inferior = self.ui.spinBox_inferior.text()
+        superior = self.ui.spinBox_superior.text()
+
+        n = api.generador_random.genera_entero_random(int(inferior), int(superior))
+
+        self.ui.label_respuesta.setText(str(n))
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    sys.exit(app.exec_())
